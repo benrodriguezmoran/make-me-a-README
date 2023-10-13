f1215cba1cce608f031b9e81a8e2758dda5a8af9
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-10-13 08:50:23 -0400

    sttarter code

--- index.js
+++ index.js
@@ -1,62 +1,80 @@
-inquirer = import('inquirer');
-// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown';
+
+const { error } = require('console');
+
+
 const questions = [
     {
-        type: 'input',
-        message: 'Enter your project title...',
-        name: 'Title',
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project:',
     },
     {
-        type: 'input',
-        message: 'Enter a description for your project...',
-        name: 'Description',
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
     },
     {
-        type: 'input',
-        message: 'Enter installation instructions...',
-        name: 'Installation',
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install your project? Provide a step-by-step description:',
     },
     {
-        type: 'input',
-        message: 'Enter usage instructions...',
-        name: 'Usage',
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use. You can include screenshots as needed:',
     },
     {
-        type: 'input',
-        message: 'Enter license details...',
-        name: 'License',
+      type: 'input',
+      name: 'credits',
+      message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence:',
     },
     {
-        type: 'input',
-        message: 'Enter contributers...',
-        name: 'Credits',
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your application:',
+      choices: ['MIT', 'Apache', 'GPL', 'None'],
     },
     {
-        type: 'input',
-        message: 'Enter some supposed FAQs...',
-        name: 'FAQs'
-    }
-    
-];
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'features',
+      message: 'If your project has a lot of features, list them here:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. You can refer to the [Contributor Covenant](https://www.contributor-covenant.org/) as an industry standard:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here:',
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-function readMeString(response) {
-    
-
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
 }
-
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-        .prompt(questions)
-        .then((response) =>
-            readMeString(response)
-        
-  );
-}
+        .createPromptModule(questions)
+        .then((answers)=> {
+            const readmeContent = generateMarkdown(answers);
+            writeToFile('README.md', readmeContent);
+            console.log('README.md successfully generated!');
+        })
+        .catch((error) => console.error(error));
+    }
 
 // Function call to initialize app
 init();
